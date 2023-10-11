102baa74e00f826d165d7e5cdb03f02773d473ed
Luca Favaretto <lucafavaretto@icloud.com>
2023-10-11 15:53:39 +0200

    task list

---
+++ js/script.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+  const btnAdd = document.getElementById("add-task");
+  btnAdd.onclick = function (e) {
+    e.preventDefault();
+    const task = document.getElementById("task");
+
+    const main = document.getElementsByTagName("main")[0];
+    console.log(main);
+
+    const newTask = task.value;
+
+    const divNewTask = document.createElement("div");
+
+    const pNewTask = document.createElement("p");
+    pNewTask.innerText = newTask;
+
+    pNewTask.onclick = function (e) {
+      const ptext = e.target;
+      ptext.className = "barred-text";
+    };
+
+    const btnNewTask = document.createElement("button");
+    btnNewTask.innerText = "Cancellami";
+
+    btnNewTask.onclick = function (e) {
+      const clickedBtn = e.target;
+      clickedBtn.parentNode.remove();
+    };
+
+    divNewTask.appendChild(pNewTask);
+    divNewTask.appendChild(btnNewTask);
+
+    main.appendChild(divNewTask);
+    task.value = "";
+  };
+};
+
+console.log(document.getElementsByTagName("main"));
